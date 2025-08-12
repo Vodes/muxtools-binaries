56be80dfd27467507fa151b7809a84de66293325
Alex H. <48185123+Vodes@users.noreply.github.com>
2025-08-12 23:42:33 +0200

    Update run-builds.py

--- run-builds.py
+++ run-builds.py
@@ -9,11 +9,11 @@ cwd = pathlib.Path()
 if len(sys.argv) < 2:
     build_files = list(cwd.rglob("build.py"))
     for file in build_files:
-        subprocess.run(["uv", "run", str(file.resolve())], shell=True)
+        subprocess.run(f'uv run "{file.resolve()}"', shell=True)
 else:
     for name in sys.argv[1:]:
         project = cwd / name
         if not project.exists() or not project.is_dir() or not (build_file := project / "build.py").exists():
             raise ValueError(f"'{name}' is not an existing or valid project!")
 
-        subprocess.run(["uv", "run", str(build_file.resolve())], shell=True)
+        subprocess.run(f'uv run "{build_file.resolve()}"', shell=True)
