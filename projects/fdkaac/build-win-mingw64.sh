08ea027143a6dbc48593e364b0a5759e2654972c
Alex H. <48185123+Vodes@users.noreply.github.com>
2025-12-27 02:04:37 +0100

    Squashed commit of the following:
    commit db9348b0e9d59c96c6a04695e6391ec4d80d5561
    Author: Alex H. <48185123+Vodes@users.noreply.github.com>
    Date:   Sat Dec 27 01:59:35 2025 +0100
    
        Pin python version to below 3.13
    
    commit 4511babab21af9f7dea680bdaaaa6e1fb58f74df
    Merge: b4e999d ca2836d
    Author: Alex H. <48185123+Vodes@users.noreply.github.com>
    Date:   Sat Dec 27 01:59:23 2025 +0100
    
        Merge branch 'main' into dev
    
    commit b4e999d750158bb0615a17c20bd3cd868163af3a
    Author: Alex H. <48185123+Vodes@users.noreply.github.com>
    Date:   Thu Aug 21 18:38:00 2025 +0200
    
        fdkaac: Delete dlls with sudo
    
    commit 0fb3cb4de51a269ab729ba675603c43d40a26346
    Author: Alex H. <48185123+Vodes@users.noreply.github.com>
    Date:   Thu Aug 21 18:35:50 2025 +0200
    
        fdkaac: Don't include dlls in zip
    
    commit a6f6edf42a825af7ba91f52431d52460739f3427
    Author: Alex H. <48185123+Vodes@users.noreply.github.com>
    Date:   Thu Aug 21 18:34:24 2025 +0200
    
        fdkaac: Remove dll files before building
    
    commit c45c53ef341c1e7feede96a92876f5564d565414
    Merge: b68be2d 57ae3d6
    Author: Alex H. <48185123+Vodes@users.noreply.github.com>
    Date:   Thu Aug 21 18:32:44 2025 +0200
    
        Merge branch 'main' into dev
    
    commit b68be2df0c2e6bcbda32334b51561f0e60aa498d
    Author: Alex H. <48185123+Vodes@users.noreply.github.com>
    Date:   Mon Aug 18 17:49:42 2025 +0200
    
        versions: Trigger fdkaac rebuild
    
    commit 2533d4ca32f643b187b8a5a5cf60d890e44b954a
    Merge: 13864ce 05b098b
    Author: Alex H. <48185123+Vodes@users.noreply.github.com>
    Date:   Mon Aug 18 17:49:25 2025 +0200
    
        Merge branch 'main' into dev
    
    commit 13864cea3dcceb921040f0f8b84c3ee51073852e
    Author: Alex H. <48185123+Vodes@users.noreply.github.com>
    Date:   Mon Aug 18 17:48:47 2025 +0200
    
        fdkaac: Include dlls for windows

--- projects/fdkaac/build-win-mingw64.sh
+++ projects/fdkaac/build-win-mingw64.sh
@@ -4,6 +4,7 @@ sudo apt-get install -y build-essential git autoconf automake libtool libtool-bi
 # Download libfdk from msys2 repos
 wget "https://repo.msys2.org/mingw/mingw64/$LIBFDK_MSYS_FILE"
 sudo tar xvf "$LIBFDK_MSYS_FILE" -C /usr/x86_64-w64-mingw32 --strip-component=1
+sudo find /usr/x86_64-w64-mingw32 -type f \( -name "libfdk-aac.dll.a" -o -name "libfdk-aac-2.dll" \) -delete
 
 # Build fdkaac
 git clone https://github.com/nu774/fdkaac.git --branch "$TAG_FDKAAC" --single-branch
